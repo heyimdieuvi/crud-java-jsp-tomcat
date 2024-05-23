df3afcaa2297f5eb263f8ccb6449f51d4f410914
Nguyen Dieu Vi <nguyendieuvi1230@gmail.com>
2024-05-24 00:46:32 +0700

    first commit

---
+++ src/java/usermanagement/dao/ConnectDB.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package usermanagement.dao;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ConnectDB {
+    private String hostName;
+    private String instance;
+    private String port;
+}
